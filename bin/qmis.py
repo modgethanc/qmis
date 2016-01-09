776722b99f6f1ef4248e0706659a32ef7c5ffb24
Vincent Zeng <hvincent@modgethanc.com>
2016-01-08 19:47:30 -0500

    basic item editing

--- bin/qmis.py
+++ bin/qmis.py
@@ -15,20 +15,12 @@ files = []
 working = "" 
 workingdir = ""
 
-
-#for x in fileinput.input():
-#    files.append(fileinput.filename())
-#    fileinput.nextfile()
-
-
 def start():
     print(header)
     print("")
     set_dir()
     print(main_menu())
     print(footer)
-
-## menus
 
 def end():
     print("")
@@ -52,6 +44,8 @@ def set_dir():
 
     return(workingdir)
 
+## menu handlers
+
 def save_file():
     print("SAVING FILE")
     print("current file list:")
@@ -65,16 +59,19 @@ def save_file():
         i += 1
     print("\t[ "+str(i)+" ] (other)")
 
-    print("\nwhere do you want to save? ", end="")
+    print("\nwhere do you want to save? (q to cancel)", end="")
     save = ""
-    choice = int(input())
-    if choice == i:
+    choice = input()
+    if choice =='q':
+        return "FIRING QUICK RELEASE"
+
+    if int(choice) == i:
         print("enter new filename: ", end="")
         save = input()
         load_dir() # refresh file list!
     else:
         save = files[choice]
-    
+
     write(save)
     return "\nsaved to "+save
 
@@ -88,13 +85,82 @@ def choose_file():
         print("\t[ "+str(i)+" ] "+x)
         i += 1
 
-    print("\npick one to load: ", end="")
-    working = int(input())
+    print("\npick one to load (q to cancel): ", end="")
+    choice = input()
+    if choice == "q":
+        return "FIRING QUICK RELEASE"
+
+    working = int(choice)
 
     return (load(files[working]))
 
+def view_detail():
+    print("single item ID: ", end="")
+    return single_item(input())
+
+def search_data():
+    print("what's your search phrase? (i'm cap sensitive, sorry) ", end="")
+    value = input()
+    print("where do you want me to look for it? (still cap sensitive) ", end="")
+    key = input()
+    result = core.find_all(datafile, key, value)
+    for x in result:
+        print(pretty_data(core.get_by_id(datafile, x)))
+    return ""
+
+def count_data():
+    print("COUNTING DATA")
+
+    statuses = core.get_all_statuses(datafile)
+    total = 0
+    for x in statuses:
+        count = len(core.get_all_status(datafile, x))
+        print(x+": "+str(count))
+        total += count
+
+    return "total: "+str(total)
+
+def short_data():
+    ids = core.get_all_ids(datafile)
+    shortdata = {}
+    for x in ids:
+        raw = core.get_by_id(datafile, x)[x]
+        item = {x:{"make":raw.get("make"), "model":raw.get("model")}}
+        shortdata.update(item)
+
+    return pretty_data(shortdata)
+
+def edit_item():
+    print("EDITING AN ITEM")
+    print("\ngive me an ID: ", end="")
+    item = input()
+
+    raw = core.get_by_id(datafile, item)[item]
+    fields = []
+    i = 0
+    for x in iter(raw):
+        print("\t[ "+str(i)+" ] "+x+": "+str(raw.get(x)))
+        i += 1
+        fields.append(x)
+
+    print("what do you want to edit? (q to cancel)", end="")
+    choice = input()
+
+    if choice =='q':
+        return "FIRING QUICK RELEASE"
+
+    key = fields[int(choice)]
+
+    print("\t"+key+": ", end="")
+    value = input()
+
+    raw.update({key:value})
+    return pretty_data(raw)
+
+## menu views
+
 def main_menu():
-    menuOptions = ["browse current dataset", "load new dataset", "save current dataset", "quit"]
+    menuOptions = ["manipulate current dataset", "load new dataset", "save current dataset", "quit"]
 
     print("")
     print("GETTING THINGS DONE")
@@ -124,11 +190,11 @@ def main_menu():
         return "FIRING QUICK RELEASE"
     else:
         print("\nno idea what you mean; you gotta pick a number from the list!")
-    
+
     return main_menu()
 
 def data_menu():
-    dataOptions = ["show raw data", "count items", "search", "back to main"]
+    dataOptions = ["show raw data", "short view", "view detail", "count items", "search", "edit item", "back to main"]
     print("")
     print("DATA BROWSING")
     print("-------------")
@@ -140,19 +206,28 @@ def data_menu():
 
     print("\ntell me your desires: ", end="")
     choice = input()
-    
+
     if choice == "0":
-        print("\n\n\n\n\n")
-        print("CURRENT DATASET:\b")
+        print("\nCURRENT DATASET:\b")
         print(pretty_data(datafile))
     elif choice == "1":
-        print("\n\n\n\n\n")
-        print(count_data())
+        print(short_data())
     elif choice == "2":
         print("\n\n\n\n\n")
-        print(search_data())
+        print(view_detail())
     elif choice == "3":
+        print("\n\n\n\n\n")
+        print(count_data())
+    elif choice == "4":
+        print("\n\n\n\n\n")
+        print(search_data())
+    elif choice == "5":
+        print("\n\n\n\n\n")
+        print(edit_item())
+    elif choice == "6":
         return "\n\n\n\n\n"
+    elif choice == 'q':
+        return "FIRING QUICK RELEASE"
     else:
         print("\nno idea what you mean; you gotta pick a number from the list!")
 
@@ -181,35 +256,14 @@ def write(filename):
 ## retrieval
 
 def random_item():
-    # return a random item
-
     ids = core.get_all_ids(datafile)
     return core.get_by_id(datafile, random.choice(ids))
 
 def pretty_data(data):
     return json.dumps(data, sort_keys=True, indent=2, separators=(',',':'))
 
-def count_data():
-    print("COUNTING DATA")
-
-    statuses = core.get_all_statuses(datafile)
-    total = 0
-    for x in statuses:
-        count = len(core.get_all_status(datafile, x))
-        print(x+": "+str(count))
-        total += count
-
-    return "total: "+str(total)
-
-def search_data():
-    print("what's your search phrase? (i'm cap sensitive, sorry) ", end="")
-    value = input()
-    print("where do you want me to look for it? (still cap sensitive) ", end="")
-    key = input()
-    result = core.find_all(datafile, key, value)
-    for x in result:
-        print(pretty_data(core.get_by_id(datafile, x)))
-    return ""
+def single_item(item):
+    return pretty_data(core.get_by_id(datafile, item))
 
 ## manipulation
 
